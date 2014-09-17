7036be76a5c9d18a7ac371d022791f3fa6e4f588
ariannaguo <ariannaguo@gmail.com>
2014-09-17 15:05:37 +0800

    built in event delegation

--- Chapter 3/03.js
+++ Chapter 3/03.js
@@ -43,11 +43,13 @@
 //        }
 //    });
 //});
-
-$('#switcher').on('click','button',function(){
-    var bodyClass=event.target.id.split('-')[1];
-    $('body').removeClass().addClass(bodyClass);
-    $('#switcher button').removeClass('selected');
-    $(this).addClass('selected');
+$(document).ready(function () {
+    $('#switcher-default').addClass('selected');
+    $('#switcher').on('click', 'button', function () {
+        var bodyClass = event.target.id.split('-')[1];
+        $('body').removeClass().addClass(bodyClass);
+        $('#switcher button').removeClass('selected');
+        $(this).addClass('selected');
+    });
 });
 
