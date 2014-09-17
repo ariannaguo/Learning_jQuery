093aab2094ed7a884e22fb3a73cb661ad69fff03
ariannaguo <ariannaguo@gmail.com>
2014-09-17 18:16:05 +0800

    complete version

--- Chapter 3/03.js
+++ Chapter 3/03.js
@@ -88,17 +88,69 @@
 //    });
 //});
 //按default的时候并不能打开／收回，按2次default再按背景，才恢复toggle的功能
-$(document).ready(function () {
-    var toggleSwitcher = function (event) {
-        if (!$(event.target).is('button')) {
+
+//$(document).ready(function () {
+//    var toggleSwitcher = function (event) {
+//        if (!$(event.target).is('button')) {
+//            $('#switcher button').toggleClass('hidden');
+//        }
+//    };
+//    $('#switcher').on('click', toggleSwitcher);
+//    $('#switcher button').click(function () {
+//        $('#switcher').off('click', toggleSwitcher);
+//        if (this.id == 'switcher-default') {
+//            $('#switcher').on('click', toggleSwitcher);
+//        }
+//    });
+//});
+
+$(document).ready(function(){
+    //Enable hover effect on the style switcher
+    $('#switcher').hover(function(){
+        $(this).addClass('hover');
+    },function(){
+            $(this).removeClass('hover');
+        });
+    //Allow the style switcher to expand and collapse
+    var toggleSwither = function(event){
+        if(!$(event.target).is('button')){
             $('#switcher button').toggleClass('hidden');
         }
     };
-    $('#switcher').on('click', toggleSwitcher);
-    $('#switcher button').click(function () {
-        $('#switcher').off('click', toggleSwitcher);
-        if (this.id == 'switcher-default') {
-            $('#switcher').on('click', toggleSwitcher);
+    $('#switcher').on('click',toggleSwither);
+    //Simulate a click so we start in a collapsed state
+    $('#switcher').click();
+    //The setBodyClass()function changes the page style
+    //The style switcher state is also updated
+    var setBodyClass = function(className){
+        $('body').removeClass().addClass(className);
+        $('#switcher button').removeClass('selected');
+        $('#switcher-'+className).addClass('selected');
+        $('#switcher').off('click',toggleSwither);
+        if(className=='default'){
+            $('#switcher').on('click',toggleSwither);
+        }
+    };
+    //Begin with the switcher-default button "selected"
+    $('#switcher-default').addClass('selected');
+    //Map key codes to their corresponding buttons to click
+    var triggers={
+        D:'default',
+        N:'narrow',
+        L:'large'
+    };
+    //Call setBodyClass() when a button is clicked
+    $('#switcher').click(function(event){
+        if($(event.target).is('button')){
+            var bodyClass= event.target.id.split('-')[1];
+            setBodyClass(bodyClass);
+        }
+    });
+    //Call setBodyClass() when a key is pressed
+    $(document).keyup(function(event){
+        var key = String.fromCharCode(event.keyCode);
+        if(key in triggers){
+            setBodyClass(triggers[key]);
         }
     });
 });
