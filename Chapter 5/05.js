b1306a7efb5f5064aaff069e121f3215f1676472
ariannaguo <ariannaguo@gmail.com>
2014-09-24 18:45:51 +0800

    chapter 5

--- Chapter 5/05.js
+++ Chapter 5/05.js
@@ -57,13 +57,94 @@
 //});
 
 //Listing 5.12
-$(document).ready(function(){
-    //insert an empty <ol> before #footer.
-    var $notes=$('<ol id="notes"></ol>').insertBefore('#footer');
-    //insert index (from 1) before each footnote in the paragraph.
-    $('span.footnote').each(function(index){
-        $('<sup>'+(index+1)+'</sup>').insertBefore(this);
-        //append the footnotes to the empty <ol>
-        $(this).appendTo($notes).wrap('<li></li>');
+//$(document).ready(function(){
+//    //insert an empty <ol> before #footer.
+//    var $notes=$('<ol id="notes"></ol>').insertBefore('#footer');
+//    //insert index (from 1) before each footnote in the paragraph.
+//    $('span.footnote').each(function(index){
+//        $('<sup>'+(index+1)+'</sup>').insertBefore(this);
+//        //append the footnotes to the empty <ol>
+//        $(this).appendTo($notes).wrap('<li></li>');
+//    });
+//});
+
+////Listing 5.16
+//$(document).ready(function(){
+//    var $notes=$('<ol id="notes"></ol>').insertBefore('#footer');
+//    $('span.footnote').each(function(index){
+//        $(this)
+//            .before([
+//                '<a href="#footnote-',
+//                index+1,
+//                '"id="context-',
+//                index+1,
+//                '" class="context">',
+//                '<sup>',
+//                index+1,
+//                '</sup></a>'
+//            ].join(''))
+//            .appendTo($notes)
+//            .append([
+//                '&nbsp;(<a href="context-',
+//                index+1,
+//                '">context</a>)'
+//            ].join(''))
+//            .wrap('<li id="footnote-'+(index+1)+'"></li>');
+//    });
+//});
+
+//complete
+$(document).ready(function() {
+    // Use attr() to add an id, rel, and title.
+    $('div.chapter a[href*="wikipedia"]').attr({
+        rel: 'external',
+        title: function() {
+            return 'Learn more about ' + $(this).text() + ' at Wikipedia.';
+        },
+        id: function(index, oldValue) {
+            return 'wikilink-' + index;
+        }
+    });
+
+    // Add "back to top" links.
+    $('<a href="#top">back to top</a>').insertAfter('div.chapter p');
+    $('<a id="top"></a>').prependTo('body');
+
+    // Create footnotes.
+    var $notes = $('<ol id="notes"></ol>').insertBefore('#footer');
+    $('span.footnote').each(function(index) {
+        $(this)
+            .before([
+                '<a href="#footnote-',
+                    index + 1,
+                '" id="context-',
+                    index + 1,
+                '" class="context">',
+                '<sup>',
+                    index + 1,
+                '</sup></a>'
+            ].join(''))
+            .appendTo($notes)
+            .append([
+                '&nbsp;(<a href="#context-',
+                    index + 1,
+                '">context</a>)'
+            ].join(''))
+            .wrap('<li id="footnote-' + (index + 1) + '"></li>');
+    });
+// Style pull quotes
+    $('span.pull-quote').each(function(index){
+        var $parentParagraph = $(this).parent('p');
+        $parentParagraph.css('position','relative');
+        var $clonedCopy = $(this).clone();
+        $clonedCopy
+            .addClass('pulled')
+            .find('span.drop')
+            .html('&hellip;')
+            .end()
+            .text($clonedCopy.text())
+            .prependTo($parentParagraph);
     });
 });
+
+
